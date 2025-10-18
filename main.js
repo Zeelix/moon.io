f48788bd5d74a63d60d00c5e0de0868288b3811a
git-engine-project <engineproject2000@gmail.com>
2025-10-18 15:36:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ window.requestAnimFrame = ( function() {
 
 var g_gl;
 var g_frame_time = {
-	counter = 30;
-	timer_last = new Date();
-	timer_current = new Date();
-	delta_t;
-	fps;
+	counter = 30,
+	timer_last = new Date(),
+	timer_current = new Date(),
+	delta_t,
+	fps
 };
 
 const html_fps = document.querySelector("#html_fps");
