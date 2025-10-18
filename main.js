02fbbabbc1608ca938d2743eeccce0256966ded7
git-engine-project <engineproject2000@gmail.com>
2025-10-18 16:01:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,76 @@
 
+// IMPORTS
 //import { rocket_modelData, rocket_modelIndexData_1, rocket_modelIndexData_2, rocket_modelIndexData_3 } from './assets.js';
 //import { star_modelData_1, star_modelData_2, star_modelIndexData } from './assets.js';
 
-//  Render
+// GLOBALS
+var g_gl;
+var g_frame_time = {
+	counter: 0,
+	timer_last: new Date(),
+	fps: 0
+};
+var g_user_held_keys = {}
+
+// HTML elements
+const html_fps = document.querySelector("#html_fps");
+const html_canvas = document.querySelector('#glcanvas');
+
 window.requestAnimFrame = ( function() {
     return  window.requestAnimationFrame || 
             window.webkitRequestAnimationFrame ||  
             window.mozRequestAnimationFrame || 
             window.oRequestAnimationFrame || 
             window.msRequestAnimationFrame ||
-    
-    // if none of the above, use non-native timeout method
     function(callback) {
       window.setTimeout(callback, 1000 / 60);
     };
   } ) (); 
 
-var g_gl;
-
-var g_frame_time = {
-	counter: 0,
-	timer_last: new Date(),
-	fps: 0
-};
-
-const html_fps = document.querySelector("#html_fps");
-
-function Render_Loop(){
-  const timer_current = new Date();
-  const delta_t = (timer_current.getTime() - g_frame_time.timer_last.getTime()) / 1000.0;
-  
-  g_frame_time.timer_last = timer_current;
-  g_frame_time.fps = (1 / delta_t).toFixed(1);
-  if(g_frame_time.counter % 10 == 0)
-	html_fps.textContent = g_frame_time.fps;
-
-  g_frame_time.counter++;
-  requestAnimFrame( Render_Loop );
-  Render(); 
+function CB_Key_Pressed(event)
+{
+	if(event.repeat) return;
+	g_user_held_keys[event.key] = true;
 }
 
-function Init() {
-	
+function CB_Key_Released(event)
+{
+	if(event.repeat) return;
+	g_user_held_keys[event.key] = false;
 }
 
-function Render() {
-	
-}
-
-function Main() {
-    const canvas = document.querySelector('#glcanvas');
-    g_gl = canvas.getContext('webgl');
+function Init() 
+{
+    g_gl = html_canvas.getContext('webgl');
 
     if (!g_gl) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.');
         return;
     }
-
-	Init();
-    Render_Loop();
+	
+	document.addEventListener('keydown', CB_Key_Pressed);
+	document.addEventListener('keyup', CB_Key_Released);
 }
 
-Main();
+function Render_Loop()
+{
+  // Timer 
+  const timer_current = new Date();
+  const delta_t = (timer_current.getTime() - g_frame_time.timer_last.getTime()) / 1000.0;
+  g_frame_time.timer_last = timer_current;
+  g_frame_time.fps = (1 / delta_t).toFixed(1);
+  if(g_frame_time.counter % 10 == 0)
+	html_fps.textContent = g_frame_time.fps;
+  g_frame_time.counter++;
+  
+  requestAnimFrame( Render_Loop );
+  Game_Update_And_Render(delta_t); 
+}
+
+function Game_Update_And_Render(t_delta_t) 
+{
+	
+}
+
+Init();
+Render_Loop();
