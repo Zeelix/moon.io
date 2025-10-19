2c91a5f9016505c6ccc8778477bd726a5c5ad3f1
git-engine-project <engineproject2000@gmail.com>
2025-10-19 13:20:48 -0700

    GPU code

--- assets.js
+++ assets.js
@@ -1,4 +1,4 @@
-export const e_asset_sm_cube_vertices = [ //T=0,N=0
+const asset_sm_cube_vertices_raw = [
 1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,
 1.0,1.0,-1.0,0.0,0.0,0.0,0.0,0.0,
 1.0,-1.0,1.0,0.0,0.0,0.0,0.0,0.0,
@@ -8,7 +8,8 @@ export const e_asset_sm_cube_vertices = [ //T=0,N=0
 -1.0,-1.0,1.0,0.0,0.0,0.0,0.0,0.0,
 -1.0,-1.0,-1.0,0.0,0.0,0.0,0.0,0.0
 ];
-export const e_asset_sm_cube_indices = [
+
+const asset_sm_cube_indices_raw = [
 6,0,4,2,0,6,
 6,4,7,4,5,7,
 4,0,5,5,0,1,
@@ -16,3 +17,6 @@ export const e_asset_sm_cube_indices = [
 3,6,7,2,6,3,
 1,2,3,0,2,1
 ];
+
+export const e_asset_sm_cube_vertices = new Float32Array(asset_sm_cube_vertices_raw);
+export const e_asset_sm_cube_indices = new Uint16Array(asset_sm_cube_indices_raw);
