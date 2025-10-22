2fd45952494d0c3ddd4b59a9ca4fffe64a5aff77
git-engine-project <engineproject2000@gmail.com>
2025-10-22 11:58:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -357,10 +357,13 @@ function Game_Update_And_Render(t_delta_t)
 	var moon_scale = vec3.fromValues(1, 1, 1);
 	var moon_translate = vec3.fromValues(0, -1, 0);
 	//mat4.translate(moon_model, moon_model, moon_translate);
-	mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[0], g_player_camera.dir_u);
-	mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[1], g_player_camera.right_u);
-	g_player_actor.pos[0] = 0.0;
-	g_player_actor.pos[1] = 0.0;
+	if(vec2.len(g_player_actor.pos) == 0)
+	{
+		mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[0], g_player_camera.dir_u);
+		mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[1], g_player_camera.right_u);
+		g_player_actor.pos[0] = 0.0;
+		g_player_actor.pos[1] = 0.0;
+	}
 	//mat4.mul(moon_model, rotation, g_player_actor.pos[1]);
 	//moon_model = mat4.copy(g_moon_local.rotation);
 	//mat4.scale(moon_model, moon_model, moon_scale);
