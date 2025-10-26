e7cbc77d5a69afd0d14a977de29e59d38c304000
git-engine-project <engineproject2000@gmail.com>
2025-10-26 16:08:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -47,17 +47,19 @@ var g_gl_ext = {
 	WEBGL_multi_draw: 0
 };
 const g_load = {
+	atomic_buffer_counters = new SharedArrayBuffer(6 * 4), // 6 int32's
+	view_buffer_counters = [];
+	
 	static_mesh_js_count: 1,
-	static_mesh_js_downloaded: 0,
-	static_mesh_js_loaded: 0,
-	
 	texture_png_count: 1,
-	texture_png_downloaded: 0,
-	texture_png_loaded: 0,
-	
 	shader_js_count: 1,
-	shader_js_downloaded: 0,
-	shader_js_compiled: 0,
+	
+	static_mesh_js_idx_downloaded: 0,
+	static_mesh_js_idx_loaded: 1,
+	texture_png_idx_downloaded: 2,
+	texture_png_idx_loaded: 3,
+	shader_js_idx_downloaded: 4,
+	shader_js_idx_compiled: 5,
 };
 const g_assets = {
 	static_mesh_js_1: null,
@@ -174,21 +176,23 @@ function Load()
 {
 	console.log('Load Start');
 	
-	g_load.static_mesh_js_downloaded = 0;
-	g_load.static_mesh_js_loaded = 0;
-	g_load.texture_png_downloaded = 0;
-	g_load.texture_png_loaded = 0;
-	g_load.shader_js_downloaded = 0;
-	g_load.shader_js_loaded = 0;
+	g_load.view_buffer_counters = new Int32Array(g_load.atomic_buffer_counters);
+	Atomics.store(g_load.view_buffer_counters, g_load.static_mesh_js_idx_downloaded, 0);
+	Atomics.store(g_load.view_buffer_counters, g_load.static_mesh_js_idx_loaded, 0);
+	Atomics.store(g_load.view_buffer_counters, g_load.shader_js_idx_downloaded, 0);
+	Atomics.store(g_load.view_buffer_counters, g_load.shader_js_idx_compiled, 0);
+	Atomics.store(g_load.view_buffer_counters, g_load.texture_png_idx_downloaded, 0);
+	Atomics.store(g_load.view_buffer_counters, g_load.texture_png_idx_loaded, 0);
 	
 	// Load Textures Async
 	g_assets.diffuse_png_1.onload = () => {
-		g_load.texture_png_downloaded = 1;
+		Atomics.add(g_load.view_buffer_counters, g_load.texture_png_idx_downloaded, 1);
 	};
 	g_assets.diffuse_png_1.onerror = () => {
 		console.log('Could not load image');
 	};
 	g_assets.diffuse_png_1.src = './diffuse_1.png';
+	
 	//  
 	//// Load Assets Async
 	//let sm_asset_1_downloaded_status = 0;
@@ -358,10 +362,11 @@ function Load()
 	//}
 	//
 	
-	//while(g_load.texture_png_downloaded != g_load.texture_png_count)
-	//{
-	//	console.log('Loading PNG...');
-	//}
+	var atomic_texture_png_downloaded_count = Atomics.load(g_load.view_buffer_counters, g_load.texture_png_idx_downloaded);
+	while(atomic_texture_png_downloaded_count != g_load.texture_png_count)
+	{
+		console.log('Loading PNG...');
+	}
 	
 	console.log('Load Complete');
 }
