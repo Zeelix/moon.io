d92d1d4f3564c921f2d33dc0dc7ac02b877e49e2
git-engine-project <engineproject2000@gmail.com>
2025-10-26 22:05:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -349,12 +349,10 @@ function Render_Loop()
   
   if(g_load.complete)
   {
-	  console.log('Scene-Game');
 	  Game_Update_And_Render_SceneGame(delta_t); 
   }
   else
   {
-	  console.log('Scene-Load');
 	  Game_Update_And_Render_SceneLoad(delta_t); 
   }
   
@@ -368,146 +366,146 @@ function Game_Update_And_Render_SceneLoad(t_delta_t)
 
 function Game_Update_And_Render_SceneGame(t_delta_t) 
 {
-	//// Update World
-	//var space_light_dir = vec3.create();
-	//vec3.rotateY(space_light_dir, g_xp_vec3, g_zero_vec3, g_space.light_theta_current);
-	//g_space.light_theta_current += t_delta_t * g_space.light_theta_speed;
-	//
-	//// Update Camera
-	//var fov_r = (Math.PI/180.0) * g_player_camera.fov_d;
-	//var fov_r_half = fov_r / 2.0;
-	//
-	//var dtheta = (g_user_mouse.x_movement_n * fov_r_half * g_player_camera.mouse_sensitivity_x);
-	//g_player_camera.actor_follow_theta = g_player_camera.actor_follow_theta - dtheta;
-	//if(g_player_camera.actor_follow_theta < 0.0)
-	//{
-	//	g_player_camera.actor_follow_theta += g_2pi;
-	//}
-	//if(g_player_camera.actor_follow_theta > g_2pi)
-	//{
-	//	g_player_camera.actor_follow_theta -= g_2pi;
-	//}
-	//
-	//const proj_aspect = g_gl.canvas.clientWidth / g_gl.canvas.clientHeight;
-	//const sin_dist = Math.sin(g_player_camera.actor_follow_theta) * g_player_camera.actor_follow_distance;
-	//const cos_dist = Math.cos(g_player_camera.actor_follow_theta) * g_player_camera.actor_follow_distance;
-	//
-	//g_player_camera.pos = vec3.fromValues(0.0, 0.0, g_player_camera.actor_follow_distance);
-	////g_player_camera.pos = vec3.fromValues(sin_dist, g_player_camera.actor_follow_height, cos_dist);
-	//mat4.perspective(g_player_camera.proj, fov_r, proj_aspect, g_player_camera.near, g_player_camera.far);
-	//mat4.lookAt(g_player_camera.view, g_player_camera.pos, g_zero_vec3, g_player_camera.global_up_u);
-	//mat4.mul(g_player_camera.view_proj, g_player_camera.proj, g_player_camera.view);
-	//
-	////var camera_dir_flat_s_inv = vec3.create();
-	////var actor_follow_height_vec3 = vec3.fromValues(0, g_player_camera.actor_follow_height, 0);
-	////vec3.rotateY(g_player_camera.dir_flat_u, g_zn_vec3, g_zero_vec3, g_player_camera.actor_follow_theta);
-	////vec3.scale(camera_dir_flat_s_inv, g_player_camera.dir_flat_u, -g_player_camera.actor_follow_distance);
-	////vec3.add(g_player_camera.pos, camera_dir_flat_s_inv, actor_follow_height_vec3);
-	//
-	//var camera_dir_u_inv = vec3.create();
-	//vec3.sub(g_player_camera.dir_u, g_zero_vec3, g_player_camera.pos);
-	//vec3.normalize(g_player_camera.dir_u, g_player_camera.dir_u);
-	//vec3.scale(camera_dir_u_inv, g_player_camera.dir_u, -1.0);
-	//
-	//vec3.cross(g_player_camera.right_u, g_player_camera.global_up_u, camera_dir_u_inv);
-	//vec3.normalize(g_player_camera.right_u, g_player_camera.right_u);
-	//vec3.cross(g_player_camera.local_up_u, camera_dir_u_inv, g_player_camera.right_u);
-	//
-	//// Update Actor
-	//var actor_is_moving = false;
-	//var actor_proj_vec2 = vec2.fromValues(0.0, 0.0);
-	//
-	//if(g_user_held_keys['w'])
-	//{
-	//	actor_is_moving = true;
-	//	vec2.add(actor_proj_vec2, actor_proj_vec2, g_yp_vec2);
-	//}
-	//if(g_user_held_keys['s'])
-	//{
-	//	actor_is_moving = true;
-	//	vec2.add(actor_proj_vec2, actor_proj_vec2, g_yn_vec2);
-	//}
-	//if(g_user_held_keys['d'])
-	//{
-	//	actor_is_moving = true;
-	//	vec2.add(actor_proj_vec2, actor_proj_vec2, g_xp_vec2);
-	//}
-	//if(g_user_held_keys['a'])
-	//{
-	//	actor_is_moving = true;
-	//	vec2.add(actor_proj_vec2, actor_proj_vec2, g_xn_vec2);
-	//}
-	//
-	//if(actor_is_moving)
-	//{
-	//	vec3.normalize(actor_proj_vec2, actor_proj_vec2);
-	//	//vec2.rotate(actor_proj_vec2, actor_proj_vec2, g_zero_vec2, g_player_camera.actor_follow_theta);
-	//	vec2.scale(actor_proj_vec2, actor_proj_vec2, g_player_actor.speed * t_delta_t);
-	//	// TODO(ED1): replace g_player_actor.pos from 3D representation to 2D surface, wrapped around the moon (uv)
-	//	//vec3.scale(g_player_actor.dir_u, g_player_camera.right_u, actor_proj_vec2[0]);
-	//	//vec3.scaleAndAdd(g_player_actor.dir_u, g_player_actor.dir_u, g_player_camera.dir_flat_u, actor_proj_vec2[1]);
-	//	//vec3.normalize(g_player_actor.dir_u, g_player_actor.dir_u);
-	//	//vec3.scale(g_player_actor.dir_s, g_player_actor.dir_u, g_player_actor.speed * t_delta_t);
-	//	//vec3.add(g_player_actor.pos, g_player_actor.pos, g_player_actor.dir_s);
-	//	vec2.add(g_player_actor.pos, g_player_actor.pos, actor_proj_vec2);
-	//}
-	//
-	//// Render
-	//g_gl.clear(g_gl.COLOR_BUFFER_BIT| g_gl.DEPTH_BUFFER_BIT);
-	//
-	//var moon_model = mat4.create();
-	//var moon_scale = vec3.fromValues(1, 1, 1);
-	//var moon_translate = vec3.fromValues(0, -1, 0);
-	////mat4.translate(moon_model, moon_model, moon_translate);
-	//
-	//let temp_quat = quat.create();
-	//quat.setAxisAngle(temp_quat, [1,0,0], g_player_actor.pos[1]);
-	//quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
-	//
-	//quat.setAxisAngle(temp_quat, [0,1,0], dtheta);
-	//quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
-	//
-	//quat.setAxisAngle(temp_quat, [0,0,1], g_player_actor.pos[0]);
-	//quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
-	//
-	////mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, dtheta, g_yp_vec3);
-	//if(vec2.len(g_player_actor.pos) != 0)
-	//{
-	//	
-	//	//mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[0], g_zp_vec3);
-	//	//mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[1], g_xp_vec3);
-	//	g_player_actor.pos[0] = 0.0;
-	//	g_player_actor.pos[1] = 0.0;
-	//}
-	////mat4.mul(moon_model, rotation, g_player_actor.pos[1]);
-	////moon_model = mat4.copy(g_moon_local.rotation);
-	////mat4.scale(moon_model, moon_model, moon_scale);
-	//
-	//mat4.fromQuat(g_moon_local.rotation_mat4, g_moon_local.rotation_quat);
-	//
-	//var moon_mvp = mat4.create();
-	//var moon_mv = mat4.create();
-	//mat4.mul(moon_mvp, g_player_camera.view_proj, g_moon_local.rotation_mat4); // usually model 
-	//mat4.mul(moon_mv, g_player_camera.view, g_moon_local.rotation_mat4); // usually model
-	//
-	//g_gl.useProgram(g_gpu.static_mesh.program_id);
-	//g_gl.bindBuffer(g_gl.ARRAY_BUFFER, g_gpu.static_mesh.vbo);
-	//g_gl.bindBuffer(g_gl.ELEMENT_ARRAY_BUFFER, g_gpu.static_mesh.ebo);
-	//
-	//const light_vi = mat3.create();
-	//const moon_mvi = mat3.create();
-	//mat3.normalFromMat4(light_vi, g_player_camera.view);
-	//mat3.normalFromMat4(moon_mvi, moon_mv);
-	//
-	//const local_light_dir = vec3.clone(space_light_dir);
-	//vec3.normalize(local_light_dir, local_light_dir);
-	//vec3.transformMat3(local_light_dir, local_light_dir, light_vi);
-	//
-	//g_gl.uniformMatrix4fv(g_gpu.static_mesh.uniform_mvp, false, moon_mvp);
-    //g_gl.uniformMatrix3fv(g_gpu.static_mesh.uniform_mvi, false, moon_mvi);
-	//g_gl.uniform3fv(g_gpu.static_mesh.uniform_light_dir, local_light_dir);
-	//
-	//g_gl.drawElements(g_gl.TRIANGLES, g_gpu.static_mesh.element_count, g_gl.UNSIGNED_SHORT, 0);
+	// Update World
+	var space_light_dir = vec3.create();
+	vec3.rotateY(space_light_dir, g_xp_vec3, g_zero_vec3, g_space.light_theta_current);
+	g_space.light_theta_current += t_delta_t * g_space.light_theta_speed;
+	
+	// Update Camera
+	var fov_r = (Math.PI/180.0) * g_player_camera.fov_d;
+	var fov_r_half = fov_r / 2.0;
+	
+	var dtheta = (g_user_mouse.x_movement_n * fov_r_half * g_player_camera.mouse_sensitivity_x);
+	g_player_camera.actor_follow_theta = g_player_camera.actor_follow_theta - dtheta;
+	if(g_player_camera.actor_follow_theta < 0.0)
+	{
+		g_player_camera.actor_follow_theta += g_2pi;
+	}
+	if(g_player_camera.actor_follow_theta > g_2pi)
+	{
+		g_player_camera.actor_follow_theta -= g_2pi;
+	}
+	
+	const proj_aspect = g_gl.canvas.clientWidth / g_gl.canvas.clientHeight;
+	const sin_dist = Math.sin(g_player_camera.actor_follow_theta) * g_player_camera.actor_follow_distance;
+	const cos_dist = Math.cos(g_player_camera.actor_follow_theta) * g_player_camera.actor_follow_distance;
+	
+	g_player_camera.pos = vec3.fromValues(0.0, 0.0, g_player_camera.actor_follow_distance);
+	//g_player_camera.pos = vec3.fromValues(sin_dist, g_player_camera.actor_follow_height, cos_dist);
+	mat4.perspective(g_player_camera.proj, fov_r, proj_aspect, g_player_camera.near, g_player_camera.far);
+	mat4.lookAt(g_player_camera.view, g_player_camera.pos, g_zero_vec3, g_player_camera.global_up_u);
+	mat4.mul(g_player_camera.view_proj, g_player_camera.proj, g_player_camera.view);
+	
+	//var camera_dir_flat_s_inv = vec3.create();
+	//var actor_follow_height_vec3 = vec3.fromValues(0, g_player_camera.actor_follow_height, 0);
+	//vec3.rotateY(g_player_camera.dir_flat_u, g_zn_vec3, g_zero_vec3, g_player_camera.actor_follow_theta);
+	//vec3.scale(camera_dir_flat_s_inv, g_player_camera.dir_flat_u, -g_player_camera.actor_follow_distance);
+	//vec3.add(g_player_camera.pos, camera_dir_flat_s_inv, actor_follow_height_vec3);
+	
+	var camera_dir_u_inv = vec3.create();
+	vec3.sub(g_player_camera.dir_u, g_zero_vec3, g_player_camera.pos);
+	vec3.normalize(g_player_camera.dir_u, g_player_camera.dir_u);
+	vec3.scale(camera_dir_u_inv, g_player_camera.dir_u, -1.0);
+	
+	vec3.cross(g_player_camera.right_u, g_player_camera.global_up_u, camera_dir_u_inv);
+	vec3.normalize(g_player_camera.right_u, g_player_camera.right_u);
+	vec3.cross(g_player_camera.local_up_u, camera_dir_u_inv, g_player_camera.right_u);
+	
+	// Update Actor
+	var actor_is_moving = false;
+	var actor_proj_vec2 = vec2.fromValues(0.0, 0.0);
+	
+	if(g_user_held_keys['w'])
+	{
+		actor_is_moving = true;
+		vec2.add(actor_proj_vec2, actor_proj_vec2, g_yp_vec2);
+	}
+	if(g_user_held_keys['s'])
+	{
+		actor_is_moving = true;
+		vec2.add(actor_proj_vec2, actor_proj_vec2, g_yn_vec2);
+	}
+	if(g_user_held_keys['d'])
+	{
+		actor_is_moving = true;
+		vec2.add(actor_proj_vec2, actor_proj_vec2, g_xp_vec2);
+	}
+	if(g_user_held_keys['a'])
+	{
+		actor_is_moving = true;
+		vec2.add(actor_proj_vec2, actor_proj_vec2, g_xn_vec2);
+	}
+	
+	if(actor_is_moving)
+	{
+		vec3.normalize(actor_proj_vec2, actor_proj_vec2);
+		//vec2.rotate(actor_proj_vec2, actor_proj_vec2, g_zero_vec2, g_player_camera.actor_follow_theta);
+		vec2.scale(actor_proj_vec2, actor_proj_vec2, g_player_actor.speed * t_delta_t);
+		// TODO(ED1): replace g_player_actor.pos from 3D representation to 2D surface, wrapped around the moon (uv)
+		//vec3.scale(g_player_actor.dir_u, g_player_camera.right_u, actor_proj_vec2[0]);
+		//vec3.scaleAndAdd(g_player_actor.dir_u, g_player_actor.dir_u, g_player_camera.dir_flat_u, actor_proj_vec2[1]);
+		//vec3.normalize(g_player_actor.dir_u, g_player_actor.dir_u);
+		//vec3.scale(g_player_actor.dir_s, g_player_actor.dir_u, g_player_actor.speed * t_delta_t);
+		//vec3.add(g_player_actor.pos, g_player_actor.pos, g_player_actor.dir_s);
+		vec2.add(g_player_actor.pos, g_player_actor.pos, actor_proj_vec2);
+	}
+	
+	// Render
+	g_gl.clear(g_gl.COLOR_BUFFER_BIT| g_gl.DEPTH_BUFFER_BIT);
+	
+	var moon_model = mat4.create();
+	var moon_scale = vec3.fromValues(1, 1, 1);
+	var moon_translate = vec3.fromValues(0, -1, 0);
+	//mat4.translate(moon_model, moon_model, moon_translate);
+	
+	let temp_quat = quat.create();
+	quat.setAxisAngle(temp_quat, [1,0,0], g_player_actor.pos[1]);
+	quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
+	
+	quat.setAxisAngle(temp_quat, [0,1,0], dtheta);
+	quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
+	
+	quat.setAxisAngle(temp_quat, [0,0,1], g_player_actor.pos[0]);
+	quat.multiply(g_moon_local.rotation_quat, temp_quat, g_moon_local.rotation_quat);
+	
+	//mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, dtheta, g_yp_vec3);
+	if(vec2.len(g_player_actor.pos) != 0)
+	{
+		
+		//mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[0], g_zp_vec3);
+		//mat4.rotate(g_moon_local.rotation, g_moon_local.rotation, g_player_actor.pos[1], g_xp_vec3);
+		g_player_actor.pos[0] = 0.0;
+		g_player_actor.pos[1] = 0.0;
+	}
+	//mat4.mul(moon_model, rotation, g_player_actor.pos[1]);
+	//moon_model = mat4.copy(g_moon_local.rotation);
+	//mat4.scale(moon_model, moon_model, moon_scale);
+	
+	mat4.fromQuat(g_moon_local.rotation_mat4, g_moon_local.rotation_quat);
+	
+	var moon_mvp = mat4.create();
+	var moon_mv = mat4.create();
+	mat4.mul(moon_mvp, g_player_camera.view_proj, g_moon_local.rotation_mat4); // usually model 
+	mat4.mul(moon_mv, g_player_camera.view, g_moon_local.rotation_mat4); // usually model
+	
+	g_gl.useProgram(g_gpu.static_mesh.program_id);
+	g_gl.bindBuffer(g_gl.ARRAY_BUFFER, g_gpu.static_mesh.vbo);
+	g_gl.bindBuffer(g_gl.ELEMENT_ARRAY_BUFFER, g_gpu.static_mesh.ebo);
+	
+	const light_vi = mat3.create();
+	const moon_mvi = mat3.create();
+	mat3.normalFromMat4(light_vi, g_player_camera.view);
+	mat3.normalFromMat4(moon_mvi, moon_mv);
+	
+	const local_light_dir = vec3.clone(space_light_dir);
+	vec3.normalize(local_light_dir, local_light_dir);
+	vec3.transformMat3(local_light_dir, local_light_dir, light_vi);
+	
+	g_gl.uniformMatrix4fv(g_gpu.static_mesh.uniform_mvp, false, moon_mvp);
+    g_gl.uniformMatrix3fv(g_gpu.static_mesh.uniform_mvi, false, moon_mvi);
+	g_gl.uniform3fv(g_gpu.static_mesh.uniform_light_dir, local_light_dir);
+	
+	g_gl.drawElements(g_gl.TRIANGLES, g_gpu.static_mesh.element_count, g_gl.UNSIGNED_SHORT, 0);
 }
 
 Load();
