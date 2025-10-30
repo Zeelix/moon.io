2d6cef5e1725f14b8740c4a0e447bd05722ed370
git-engine-project <engineproject2000@gmail.com>
2025-10-29 17:28:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -85,8 +85,8 @@ var g_player_actor = {
 	velocity: vec2.fromValues(0.0, 0.0),
 	accel: vec2.fromValues(0.0, 0.0),
 	dir_u: vec3.fromValues(0.0, 0.0, -1.0),
-	speed: 0.5,
-	friction: 1.0
+	speed: 30.0,
+	friction: 0.01
 };
 var g_player_camera = {	
 	global_up_u: vec3.fromValues(0.0, 1.0, 0.0),
