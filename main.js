0a1b5b720f99b9b72e9140d6fdc9c96d6808abd7
git-engine-project <engineproject2000@gmail.com>
2025-11-07 16:11:18 -0800

    Update main.js

--- main.js
+++ main.js
@@ -442,11 +442,11 @@ function CB_Mouse_Move(event)
 					var bay_k = Clamp(Math.floor(vec3.dot(intersect_point, g_ico_collider.face_change_of_base_k[closest_surface_index]) * 8 + 0.5), 0, 7);
 					console.log('V(', bay_i, ',', bay_j, ',', bay_k, ')');
 					
-					face_index_buffer_view.setUint8(0, bay_i);
-					face_index_buffer_view.setUint8(1, bay_j);
-					face_index_buffer_view.setUint8(2, bay_k);
+					g_ico_collider.face_index_buffer_view.setUint8(0, bay_i);
+					g_ico_collider.face_index_buffer_view.setUint8(1, bay_j);
+					g_ico_collider.face_index_buffer_view.setUint8(2, bay_k);
 					
-					var closest_subface_key = face_index_buffer_view.getUint32(0);
+					var closest_subface_key = g_ico_collider.face_index_buffer_view.getUint32(0);
 					var closest_subface_index = g_ico_collider.face_index_lookup.indexOf(closest_subface_key);
 					
 					//var bay_i_v = Clamp(Math.floor(vec3.dot(intersect_point, g_ico_collider.face_change_of_base_i[closest_surface_index]) * 8), 0, 7);
@@ -508,7 +508,7 @@ function Load()
 	html_canvas.addEventListener('mousemove', CB_Mouse_Move);
 	html_canvas.addEventListener('click', CB_Mouse_Click);
 	
-	g_ico_collider.face_index_buffer_view = new DataView(g_g_ico_collider.face_index_buffer);
+	g_ico_collider.face_index_buffer_view = new DataView(g_ico_collider.face_index_buffer);
 	g_ico_collider.face_index_buffer_view.setUint8(3,0);
 	
 	// GL Render-Context
