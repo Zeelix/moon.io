b86f386fb6fa71d70defc4cdd7dcdadc8a018b83
git-engine-project <engineproject2000@gmail.com>
2025-11-08 19:17:17 -0800

    Update main.js

--- main.js
+++ main.js
@@ -629,7 +629,7 @@ function Load()
 		
 		Check_Game_Is_Loaded();
 	};
-	g_assets.diffuse_png_1.src = 'diffuse_7.png';
+	g_assets.diffuse_png_1.src = 'diffuse_6.png';
 	
 	// Load Assets Async
 	const shaders_js_1_promise = import('./static_mesh_1.js');
